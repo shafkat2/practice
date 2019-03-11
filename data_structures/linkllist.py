d0073db768099a6f03cddee8de27286190f8fbeb
shafkat2 <shafkat.waheed@northsouth.edu>
2019-03-12 03:33:20 +0600

    linklist_structure

---
+++ data_structures/linkllist.py
@@ -0,0 +1,33 @@
+class node(object):
+
+    def __init__(self, data):
+        self.data = data
+        self.nextNode = None
+
+class LinkedList(object):
+
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def insertStart(self,data):
+
+        self.size = self.size + 1
+        newNode = Node(data)
+
+        if not self.head:
+            self.head = newNode
+        else:
+            newNode.nextNode = self.head
+            self.head = newNode
+
+    def size(self):
+        return self.size
+    def size2(self):
+        actualNode = self.head
+        size = 0
+        while actualNode is not None:
+            size+ = 1
+            actualNode - actualNode.nextNode
+        
+        return size
