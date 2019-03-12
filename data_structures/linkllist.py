685484d68a508d77118097bc7811e6fc776a647d
shafkat2 <shafkat.waheed@northsouth.edu>
2019-03-13 02:52:54 +0600

    remove added

--- data_structures/linkllist.py
+++ data_structures/linkllist.py
@@ -50,3 +50,21 @@ class LinkedList(object):
         while actualNode is not None:
             print("%d",actualNode)
             acutalNode = acutalNode.nextNode
+    
+    def remove(self, data):
+
+        if self.head is None:
+            return
+
+        self.size = self.size -1
+        
+        currentNode = self.head
+        previousNode = None
+        
+        while currentNode.data != data:
+            previousNode = currentNode
+            currentNode = currentNode.nextNode
+        if(previousNode is None):
+            self.head = currentNode.nextNode
+        else:
+            previousNode.newNode = currentNode.nextNode    
