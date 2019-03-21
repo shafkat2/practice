b488bec2818433ff757e281473719972b245afd1
shafkat2 <shafkat.waheed@northsouth.edu>
2019-03-22 02:24:32 +0600

    bst tress

--- data_structures/BST.py
+++ data_structures/BST.py
@@ -1,5 +1,5 @@
 
-class Node(objext):
+class Node(object):
     def __init__(self,data):
         self.data = data
         self.leftchild = None
@@ -7,14 +7,16 @@ class Node(objext):
 
     
 
-class bnarySearchTree(object):
+class binarySearchTree(object):
 
     def __init__(self,):
+        self.root = None
+    
+    def insert(self,data):
         if not self.root:
             self.root = Node(data)
         else:
-            self.insertNode(data,self.root)
-
+            self.insertNode(data,self.root)    
     #O(logN) ---- only for balanced tree
     def insertNode(self,data,node):
 
@@ -27,7 +29,7 @@ class bnarySearchTree(object):
             if node.rightchild:
                 self.insertNode(data, node.rightchild)
             else:
-                node.rightchild = node(data)
+                node.rightchild = Node(data)
     
     def getMinValue(self):
         if self.root:
@@ -62,4 +64,55 @@ class bnarySearchTree(object):
 
         if node.rightchild:
             self.traverseinOrder(node.rightchild)
+
+    def removeNode(self,data,node):
+         if not node:
+             return node
+
+         if data < node.data:
+            node.leftchild = self.removeNode(data, node.leftchild)
+         elif data >node.data:
+             node.rightchild = self.removeNode(data,node.rightchild)
+         else:
+
+             if not node.leftchild and not node.rightchild:
+                 print("removing a leaf node")
+                 del node
+                 return None
+             if not node.leftchild:
+                 print("removing a node with left child")
+                 tempNode = node.leftchild
+                 del node
+                 return tempNode
+            
+             elif not node.rightChild:
+                 print("removing a node with right child")
+                 tempNode = node.leftchild
+                 del node
+                 return tempNode
+
+             print("removing node with two child")
+             tempNode = self.getPredecessor(node.leftchild)
+             node.data = tempNode.data 
+             node.leftchild = self.removeNode(tempNode.data, node.leftchild)
+
+         return node
+    def getPredecessor(self,node):
+
+         if node.rightChild:
+             return self.getPredecessor(node.leftchild)      
+    
+    def remove(self,data):
+        if self.root:
+            self.root = self.removeNode(data,self.root)
         
+
+
+bst = binarySearchTree()
+bst.insert(10)
+bst.insert(5)
+bst.insert(14)
+bst.remove(5)
+
+bst.traverse()
+print(bst.getMinValue())
