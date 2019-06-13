c1f5cef2796e4bd4a364563244e43c0c80084ea5
shafkat2 <shafkat.waheed@northsouth.edu>
2019-06-14 05:46:13 +0600

    genetic algortihm

--- data_structures and algorithms/Genetic_algo/Population.java
+++ data_structures and algorithms/Genetic_algo/Population.java
@@ -1,3 +1,44 @@
 package Genetic_algo;
 
 
+package Genetic_algo;
+
+
+public class Population{
+
+        private Individuals[] individual;
+
+        public Population(int populationSize){
+            individual = new Individuals[populationSize];
+        }
+        public void initialize(){
+            for(int i = 0; i<individual.length;++i){
+                Individuals newIndividual = new Individuals();
+                newIndividual.generateIndividual();
+                saveIndividual(i,newIndividual);
+            }
+        }
+        
+    private void saveIndividual(int i, Individuals newIndividual) {
+        this.individual[i] = newIndividual;
+    }
+
+    public Individuals getIndividual(int index) {
+            return this.individual[index];
+        }
+
+        public Individuals getFittestIndividuals(){
+            Individuals fittest = individual[0];
+
+            for(int i =1; i<individual.length;++i){
+                if(getIndividual(i).getFitness()>= fittest.getFitness())
+                    fittest = getIndividual(i);
+            }
+            return fittest;
+        }
+
+    public int size(){
+        return this.individual.length;
+    }    
+
+}
